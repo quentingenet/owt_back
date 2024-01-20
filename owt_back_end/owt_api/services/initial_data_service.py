071b9a2974e187f467fad9c899450ce04e13fa5f
Quentin Genet <genetquentin@gmail.com>
2024-01-20 09:11:31 +0100

    🚀initial commit and initializing OWT API DjangoREST

---
+++ owt_back_end/owt_api/services/initial_data_service.py
@@ -0,0 +1,12 @@
+from owt_api.models import InitialData
+
+
+def initial_data_logic(data):
+    pass
+<<<<<<< HEAD
+
+
+def set_initial_data_first_connexion(data):
+    return None
+=======
+>>>>>>> ce2b312 (Feature 'register' in progress)
